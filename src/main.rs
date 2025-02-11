ccbe5417576799f65d572bc98bc690c71eed1c34
LongCoderX <skrlong1997@gmail.com>
2025-02-11 14:50:01 +0800

    添加命令行相关的内容，用于区分不同模块的联系

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,38 @@ mod ownership;
 mod structrue;
 mod collector;
 
+use clap::Parser;
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long = "pattern")]
+    pattern: String,
+}
+
 fn main() {
-    /* 所有权学习 */
-    ownership::base::ownership();
-    ownership::base::function_ownership();
-    ownership::base::return_val_ownership();
+    let args = Cli::parse();
 
-    ownership::reference::func_arg_reference();
+    match args.pattern.as_str() {
+        "ownership" => {
+            /* 所有权学习 */
+            ownership::base::ownership();
+            ownership::base::function_ownership();
+            ownership::base::return_val_ownership();
+            ownership::reference::func_arg_reference();
+            ownership::slice::slice_function();
+        },
+        "structrue" => {
+            /* 结构体 */
+            structrue::base::print_struct();
+            structrue::rectangles::calculate_rect();
+        },
+        "collector" => {
+            /* 容器 */
+            collector::vectorcollector::print_vector_collector();
+            collector::hashmapcollector::print_hashmap_collector();
+        },
+        _ => {
+            panic!("Not pattern");
+        },
+    }
 
-    ownership::slice::slice_function();
-
-    /* 结构体 */
-    structrue::base::print_struct();
-    structrue::rectangles::calculate_rect();
-
-    /* 容器 */
-    collector::vectorcollector::print_vector_collector();
-    collector::hashmapcollector::print_hashmap_collector();
 }
